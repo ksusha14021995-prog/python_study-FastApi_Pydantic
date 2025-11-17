378e128f6e7031390f6c70b27be8b0c870ebe3ec
Ksenia <Ksusha14031995@gmail.com>
2025-11-18 02:46:00 +0300

    Задание 1

---
+++ main.py
@@ -0,0 +1,57 @@
+import uvicorn
+import re
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr, field_validator
+from datetime import date
+import json
+
+
+class Student(BaseModel):
+    surname: str
+    name: str
+    birthday_date: date
+    phone: str
+    email: EmailStr
+
+    @field_validator("name")
+    def validate_name(cls, value: str) -> str:
+        pattern = r"^[А-ЯЁ][а-яё]+$"
+        if not re.match(pattern, value):
+            raise ValueError("Имя должно начинаться с заглавной буквы и содержать только кириллицу")
+        return value
+
+    @field_validator("surname")
+    def validate_surname(cls, value: str) -> str:
+        pattern = r"^[А-ЯЁ][а-яё]+$"
+        if not re.match(pattern, value):
+            raise ValueError("Фамилия должна начинаться с заглавной буквы и содержать только кириллицу")
+        return value
+
+    @field_validator("phone")
+    def validate_phone(cls, value: str) -> str:
+        pattern = r"^[0-9\-\+]{9,15}$"
+        if not re.match(pattern, value):
+            raise ValueError("Телефон должен быть введен по маске")
+        return value
+
+app = FastAPI()
+
+@app.get("/health")
+async def health():
+    return {"mes": "ok"}
+
+@app.post("/add_student")
+async def add(model:Student):
+    data = model.model_dump(mode="json")
+    with open("student.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False)
+    return model.model_dump()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "__main__:app",
+        host='0.0.0.0',
+        port=8000,
+        reload=True
+    )
+
